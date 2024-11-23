6cd118decff1a27299998fe4b1554a06bbacd7cf
Kitsuensaki Mezumona <mezumona@gmail.com>
2024-11-23 14:08:43 +0900

    fix: Support for both classic-style and SDK-style csproj

--- src/CsprojModifier/Assets/CsprojModifier/Editor/Features/InsertAdditionalImportFeature.cs
+++ src/CsprojModifier/Assets/CsprojModifier/Editor/Features/InsertAdditionalImportFeature.cs
@@ -144,7 +144,13 @@ namespace CsprojModifier.Editor.Features
 
                 var baseDir = Path.GetDirectoryName(path);
                 var xDoc = XDocument.Parse(content);
-                var projectE = xDoc.Element("Project");
+                var nsMsbuild = (XNamespace)"http://schemas.microsoft.com/developer/msbuild/2003";
+                var projectE = xDoc.Element(nsMsbuild + "Project") ?? xDoc.Element("Project");
+                if (projectE == null)
+                {
+                    throw new Exception($"Unknown csproj format: {path}");
+                }
+                var projectNs = projectE.GetDefaultNamespace();
 
                 foreach (var target in settings.AdditionalImports)
                 {
@@ -153,11 +159,11 @@ namespace CsprojModifier.Editor.Features
                     if (target.Position == ImportProjectPosition.Append)
                     {
                         projectE.Add(new XComment($"{target.Path}:{hash}"));
-                        projectE.Add(new XElement("Import", new XAttribute("Project", target.Path)));
+                        projectE.Add(new XElement(projectNs + "Import", new XAttribute("Project", target.Path)));
                     }
                     else if (target.Position == ImportProjectPosition.Prepend)
                     {
-                        projectE.AddFirst(new XElement("Import", new XAttribute("Project", target.Path)));
+                        projectE.AddFirst(new XElement(projectNs + "Import", new XAttribute("Project", target.Path)));
                         projectE.AddFirst(new XComment($"{target.Path}:{hash}"));
                     }
                     else if (target.Position == ImportProjectPosition.AppendContent)
